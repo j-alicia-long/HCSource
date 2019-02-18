d51357b5996f2ccf216386c48e2832283fd31a47
Jennifer Long <j.alicia.long@gmail.com>
2019-02-18 17:45:35 -0500

    doggy update

--- dogyears.js
+++ dogyears.js
@@ -4,16 +4,24 @@ Puppy Age Calculator
 */
 
 //ES6 function syntax
-const calculateDogAge = (humanAge, ratio) => {
-  return humanAge*ratio;
+const calculateDogAge = (age, ratio, humanToDog=true) => {
+  return humanToDog ? age*ratio : age/ratio;
 };
 
 const main = () => {
-  const ages = [5, 8, 3];
+  const humanYears = [5, 8, 3];
+  const dogYears = [25, 18, 73];
   const ratio = 7;
-  ages.forEach(function(age){
-    console.log(`Your doggie is ${calculateDogAge(age, ratio)} years old in dog years!`);
+  // human years to dog years
+  humanYears.forEach(function(age){
+    console.log(`Your ${age} year old doggie is ${calculateDogAge(age, ratio, true)} years old in dog years!`);
   });
+  // dog years to human years
+  dogYears.forEach(function(age){
+    console.log(`Your ${age} year old doggie is ${calculateDogAge(age, ratio, false)} years old in human years!`);
+  });
+  // Default is human to dog
+  console.log(`Your 7 year old doggie is ${calculateDogAge(7, ratio)} years old in dog years!`);
 };
 
 main();
